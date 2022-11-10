9efd43423d189c2208e9161a331dbb6c1d49bf84
MrGogoll <115408973+MrGogoll@users.noreply.github.com>
2022-11-10 20:10:11 +0300

    17

---
+++ 5.cpp
@@ -0,0 +1,15 @@
+﻿// №5 Ввести целое число n, вывести в консоль числа от 1 до n. Использовать цикл for()
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		cout << i + 1 << endl;
+	}
+
+}
+
